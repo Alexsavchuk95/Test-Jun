c86ca84f45eed1b167042b71b4d159403f24434c
AlexSavchuk95 <savchukalex57@gmail.com>
2020-08-13 15:02:02 +0300

    Test-Jun

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('#menu-togle');
+const mobileNavContainer = document.querySelector('#mobile-nav');
+
+menuToggle.onclick = function(){
+    menuToggle.classList.toggle('menu-icon-active');
+    mobileNavContainer.classList.toggle('menu-list__mobile--active');
+}
